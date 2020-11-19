f53c0305047ee171343e8477845c6400cb47680f
mgurga <motiurga@gmail.com>
2020-11-19 13:31:36 -0800

    created basic grid and title screen

---
+++ app/src/main/java/com/tictactoe/MainActivity.java
@@ -0,0 +1,19 @@
+package com.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void playButtonPressed(View view) {
+        setContentView(R.layout.gamegrid);
+    }
+}
