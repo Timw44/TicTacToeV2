53320486df03844cf127c616cac4819cfa1a9fcd
Jaqueline Gomez <91078781@mvusd.net>
2021-03-08 21:11:43 -0800

    unit tests for WinChecker class added

--- app/src/test/java/com/tictactoe/ExampleUnitTest.java
+++ app/src/test/java/com/tictactoe/ExampleUnitTest.java
@@ -10,8 +10,85 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void noWinner()
+    {
+        int[][] tttboard = {{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = -1;
+       assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void noWinner2()
+    {
+        int[][] tttboard = {{0,-1,-1},{-1,-1,-1},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = -1;
+        assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void noWinner3()
+    {
+        int[][] tttboard = {{0,0,-1},{-1,-1,-1},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = -1;
+        assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void noWinner4()
+    {
+        int[][] tttboard = {{-1,-1,0},{-1,-1,-1},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = -1;
+        assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void noWinner5()
+    {
+        int[][] tttboard = {{-1,-1,-1},{0,-1,-1},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = -1;
+        assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void noWinner6()
+    {
+        int[][] tttboard = {{-1,-1,-1},{-1,0,-1},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = -1;
+        assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void noWinner7()
+    {
+        int[][] tttboard = {{-1,-1,-1},{-1,-1,0},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = -1;
+        assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void Winner()
+    {
+        int[][] tttboard = {{1,1,1},{-1,-1,-1},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = 1;
+        assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void winner2()
+    {
+        int[][] tttboard = {{1,1,1},{-1,-1,-1},{-1,-1,-1}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = 1;
+        assertEquals(expected,noWinner.checkWinner());
     }
 }
