06bd85006d3068500442ebf2f1c5e288100aa6f5
Jaqueline Gomez <91078781@mvusd.net>
2021-03-23 13:18:16 -0700

    unit tests for WinChecker class added

--- app/src/test/java/com/tictactoe/ExampleUnitTest.java
+++ app/src/test/java/com/tictactoe/ExampleUnitTest.java
@@ -16,8 +16,8 @@ public class ExampleUnitTest {
     {
         int[][] tttboard = {{-1,-1,-1},{-1,-1,-1},{-1,-1,-1}};
         WinChecker noWinner=new WinChecker(tttboard);
-        int expected = -1;
-       assertEquals(expected,noWinner.checkWinner());
+        int expected = 1;
+        assertEquals(expected,noWinner.checkWinner());
     }
 
     @Test
@@ -25,7 +25,7 @@ public class ExampleUnitTest {
     {
         int[][] tttboard = {{0,-1,-1},{-1,-1,-1},{-1,-1,-1}};
         WinChecker noWinner=new WinChecker(tttboard);
-        int expected = -1;
+        int expected = 1;
         assertEquals(expected,noWinner.checkWinner());
     }
 
@@ -34,7 +34,7 @@ public class ExampleUnitTest {
     {
         int[][] tttboard = {{0,0,-1},{-1,-1,-1},{-1,-1,-1}};
         WinChecker noWinner=new WinChecker(tttboard);
-        int expected = -1;
+        int expected = 1;
         assertEquals(expected,noWinner.checkWinner());
     }
 
@@ -43,7 +43,7 @@ public class ExampleUnitTest {
     {
         int[][] tttboard = {{-1,-1,0},{-1,-1,-1},{-1,-1,-1}};
         WinChecker noWinner=new WinChecker(tttboard);
-        int expected = -1;
+        int expected = 1;
         assertEquals(expected,noWinner.checkWinner());
     }
 
@@ -52,7 +52,7 @@ public class ExampleUnitTest {
     {
         int[][] tttboard = {{-1,-1,-1},{0,-1,-1},{-1,-1,-1}};
         WinChecker noWinner=new WinChecker(tttboard);
-        int expected = -1;
+        int expected = 1;
         assertEquals(expected,noWinner.checkWinner());
     }
 
@@ -61,7 +61,7 @@ public class ExampleUnitTest {
     {
         int[][] tttboard = {{-1,-1,-1},{-1,0,-1},{-1,-1,-1}};
         WinChecker noWinner=new WinChecker(tttboard);
-        int expected = -1;
+        int expected = 1;
         assertEquals(expected,noWinner.checkWinner());
     }
 
@@ -70,25 +70,216 @@ public class ExampleUnitTest {
     {
         int[][] tttboard = {{-1,-1,-1},{-1,-1,0},{-1,-1,-1}};
         WinChecker noWinner=new WinChecker(tttboard);
-        int expected = -1;
+        int expected = 1;
         assertEquals(expected,noWinner.checkWinner());
     }
 
     @Test
-    public void Winner()
+    public void winnerHorizontal()
     {
-        int[][] tttboard = {{1,1,1},{-1,-1,-1},{-1,-1,-1}};
+        int[][] tttboard = {{1,1,1},
+                {-1,-1,-1},
+                {-1,-1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerHorizontal2()
+    {
+        int[][] tttboard = {{-1,-1,-1},
+                {1,1,1},
+                {-1,-1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerHorizontal3()
+    {
+        int[][] tttboard = {{-1,-1,-1},
+                {-1,-1,-1},
+                {1,1,1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerVertical()
+    {
+        int[][] tttboard = {{1,-1,-1},
+                {1,-1,-1},
+                {1,-1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerVertical2()
+    {
+        int[][] tttboard = {{-1, 1,-1},
+                {-1, 1,-1},
+                {-1, 1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerVertical3()
+    {
+        int[][] tttboard = {{-1, -1,1},
+                {-1, -1,1},
+                {-1, -1,1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerDiagonal()
+    {
+        int[][] tttboard = {{-1, -1,1},
+                {-1, 1,-1},
+                {1, -1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerDiagonal2()
+    {
+        int[][] tttboard = {{1, -1,-1},
+                {-1, 1,-1},
+                {-1, -1,1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void noWinner8()
+    {
+        int[][] tttboard = {{0,-1,-1},
+                {-1,-1,1},
+                {-1,0,-1}};
         WinChecker noWinner=new WinChecker(tttboard);
         int expected = 1;
         assertEquals(expected,noWinner.checkWinner());
     }
 
     @Test
-    public void winner2()
+    public void noWinner9()
     {
-        int[][] tttboard = {{1,1,1},{-1,-1,-1},{-1,-1,-1}};
+        int[][] tttboard = {{0,0,1},
+                {1,1,0},
+                {0,1,0}};
         WinChecker noWinner=new WinChecker(tttboard);
         int expected = 1;
         assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void noWinner10()
+    {
+        int[][] tttboard = {{0,1,0},
+                {0,1,1},
+                {1,0,0}};
+        WinChecker noWinner=new WinChecker(tttboard);
+        int expected = 1;
+        assertEquals(expected,noWinner.checkWinner());
+    }
+
+    @Test
+    public void winnerHorizontal_wing()
+    {
+        int[][] tttboard = {{0,0,0},
+                {-1,-1,-1},
+                {-1,-1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerHorizontal2_wing()
+    {
+        int[][] tttboard = {{-1,-1,-1},
+                {0,0,0},
+                {-1,-1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerHorizontal3_wing()
+    {
+        int[][] tttboard = {{-1,-1,-1},
+                {-1,-1,-1},
+                {0,0,0}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerVertical_wing()
+    {
+        int[][] tttboard = {{0,-1,-1},
+                {0,-1,-1},
+                {0,-1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerVertical2_wing()
+    {
+        int[][] tttboard = {{-1, 0,-1},
+                {-1, 0,-1},
+                {-1, 0,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerVertical3_wing()
+    {
+        int[][] tttboard = {{-1, -1,0},
+                {-1, -1,0},
+                {-1, -1,0}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerDiagonal_wing()
+    {
+        int[][] tttboard = {{-1, -1,0},
+                {-1, 0,-1},
+                {0, -1,-1}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
+    }
+
+    @Test
+    public void winnerDiagonal2_wing()
+    {
+        int[][] tttboard = {{0, -1,-1},
+                {-1, 0,-1},
+                {-1, -1,0}};
+        WinChecker winner=new WinChecker(tttboard);
+        int expected = 0;
+        assertEquals(expected,winner.checkWinner());
     }
 }
